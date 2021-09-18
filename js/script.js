6bc105770abb6839e7348b5474dcbd4eb4ccae85
matiaserodriguez <maturod96@gmail.com>
2021-09-18 16:17:19 -0300

    updating last things

--- js/script.js
+++ js/script.js
@@ -6,16 +6,16 @@ const day = today.getDate();
 const month = today.getMonth() + 1;
 const year = today.getFullYear();
 
-console.log(`${day} ${month} ${year}`);
-
 // get the hours, minutes and seconds
 const hours = today.getHours();
 const minutes = today.getMinutes();
 const seconds = today.getSeconds();
+
+const modified = document.lastModified;
 
 // takes the last-updated class from html
 const lastUpdated = document.querySelector(".last-updated");
 
 document.getElementById("date-and-time").textContent = year;
 
-lastUpdated.textContent = `Last Updated ${month}/${day}/${year} ${hours}:${minutes}:${seconds}`;
+lastUpdated.textContent = `Last Updated ${modified}`;
