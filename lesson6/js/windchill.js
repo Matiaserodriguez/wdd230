aaa5814856f5d78cf0dbeba63726626a34c6bfe1
matiaserodriguez <maturod96@gmail.com>
2021-10-23 22:30:12 -0300

    updating farenheit formula on lesson6 windchil

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -8,7 +8,7 @@ const windchill =
   35.75 * wind ** 0.16 +
   0.4275 * farenheit * wind ** 0.16;
 
-if (farenheit >= 50 && wind >= 4.8) {
+if (farenheit <= 50 && wind >= 4.8) {
   calculation.textContent = Math.round(windchill);
 } else {
   calculation.textContent = "N/A";
