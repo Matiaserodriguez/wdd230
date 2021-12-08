16cf0185912945498c73fc69c363b8df3afe949f
matiaserodriguez <maturod96@gmail.com>
2021-12-08 12:13:49 -0300

    modifies .jpg to .png on JSON

--- lesson12-13-14/js/script.js
+++ lesson12-13-14/js/script.js
@@ -1,5 +1,7 @@
 const hamburger = document.querySelector(".ham");
 const mainnav = document.querySelector(".navbar-items");
+const baseURL =
+  "https://matiaserodriguez.github.io/wdd230/lesson12-13-14/json/stores.json";
 
 hamburger.addEventListener(
   "click",
@@ -12,3 +14,34 @@ hamburger.addEventListener(
 window.onresize = () => {
   if (window.innerWidth > 600) mainnav.classList.remove("responsive");
 };
+
+const storesJSON = async (key) => {
+  try {
+    const response = await fetch(`${baseURL}`);
+    const wholeResponse = await response.json();
+    let theList = wholeResponse[key];
+    theList.forEach((element) => {
+      console.log(element);
+      const sectionCards = document.querySelector(".container-home");
+      const theCard = document.createElement("div");
+      theCard.setAttribute("class", "card");
+      let h3 = document.createElement("h3");
+      let image = document.createElement("img");
+      let link = document.createElement("p");
+      h3.innerHTML = element.name;
+      image.setAttribute("src", `images/${element.logo}`);
+      image.setAttribute("alt", `${element.name}'s photo`);
+      link.innerHTML = `<a href="${element.link}">Link to the page<a>`;
+      // image.setAttribute("class", "card-photo");
+      theCard.appendChild(h3);
+      theCard.appendChild(image);
+      theCard.appendChild(link);
+      sectionCards.appendChild(theCard);
+    });
+    return theList;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+storesJSON("stores");
